37f8e3d479e94c2030da97de5e0d8618e524607c
Aditya Lesmana <alesmana@gmail.com>
2012-02-29 17:58:16 +0800

    Up to exercise 3 extra credit

--- ex/ex1.py
+++ ex/ex1.py
@@ -1,4 +1,4 @@
-print "Hello World!"
+﻿print "Hello World!"
 print "Hello Again"
 print "I like typing this."
 print "This is fun"
---
+++ ex/ex3.py
@@ -0,0 +1,33 @@
+print "I will count my chickens:"
+
+# comma can give some kind of concat operation
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+# print the result of calculation
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# print the result of boolean ops
+print 3 + 2 < 5 < 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2 
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+# Extra: 
+# use floating point numbers so it's more accurate 
+# (hint: 20.0 is floating point).
+
+# print the result of calculation
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
